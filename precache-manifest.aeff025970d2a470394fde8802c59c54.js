ae8c177a58540484a5470e7298c3ce83333bb003
kleshninpo <kleshnin.p.o@gmail.com>
2018-12-13 00:16:09 +0300

    Updates

---
+++ precache-manifest.aeff025970d2a470394fde8802c59c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77084a5745fd76fb771e",
+    "url": "/aviaSchedule/static/js/runtime~main.77084a57.js"
+  },
+  {
+    "revision": "724312f21430e02951a7",
+    "url": "/aviaSchedule/static/js/main.724312f2.chunk.js"
+  },
+  {
+    "revision": "9815bba2b18e667b7a0a",
+    "url": "/aviaSchedule/static/js/1.9815bba2.chunk.js"
+  },
+  {
+    "revision": "724312f21430e02951a7",
+    "url": "/aviaSchedule/static/css/main.4c35b8a8.chunk.css"
+  },
+  {
+    "revision": "ab76d9823246cd5233b0e8aec8ba85f2",
+    "url": "/aviaSchedule/index.html"
+  }
+];
